835c015b7426e5d47b71d4a8ac2b7a8d0b13171e
bigazzzz <bigazzzz@gmail.com>
2024-01-04 02:55:43 +0300

    v1.0

---
+++ auction.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+bold=$(tput bold)
+underline=$(tput smul)
+italic=$(tput sitm)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+red=$(tput setaf 1)
+reset=$(tput sgr0)
+
+SLEEPTIME=60
+MESSAGE_COLOR=$reset
+
+echo "Enter your credentials on simplecloud.ru"
+read -p "Login: " LOGIN
+read -s -p "Password: " PASSWORD
+BEARER_TOKEN=$(curl -d "{\"login\":\"$LOGIN\",\"password\":\"$PASSWORD\"}" -X POST -H'Content-Type:application/json;charset=UTF-8' -s https://simplecloud.ru/api/v3/auth/login | jq -r '.session_key')
+echo
+
+PRICE_START=$(curl -s -H"Authorization: Bearer $BEARER_TOKEN" https://simplecloud.ru/api/v3/auction\?per_page\=1000 | jq -r '.lots[0].start_price' | sed 's/\"//g' | sed 's/\.00//g')
+
+echo "${green}----------------------------------------------------------------------"
+curl -s -H"Authorization: Bearer $BEARER_TOKEN" https://simplecloud.ru/api/v3/auction\?per_page\=1000 | jq -r '.lots[0] | @sh "Lot price - \(.start_price). Started at \(.started)\nLot config - \(.size.vcpus)/\(.size.ram)/\(.size.disk)"' | tr -d "'"
+echo "----------------------------------------------------------------------${reset}"
+
+echo "**********************************************************************"
+echo "|        DATE         | PERIOD | YEAR PRICE | MONTH PRICE | DISCOUNT |"
+while [ 1=1 ]; do
+  PRICE=$(curl -s -H"Authorization: Bearer $BEARER_TOKEN" https://simplecloud.ru/api/v3/auction\?per_page\=1000 | jq '.lots[0] .price' | sed 's/\"//g' | sed 's/\.00//g')
+  let PRICE_DISCOUNT=100-$PRICE*100/$PRICE_START
+  let PRICE_MONTH=$PRICE/12
+  printf "\r${MESSAGE_COLOR}| $(date +"%T %m-%d-%Y") | %5ds | %6d RUB | %7d RUB | %7d%% |${reset}" $SLEEPTIME $PRICE $PRICE_MONTH $PRICE_DISCOUNT
+  if [ $PRICE_DISCOUNT -ge 40 ]; then
+    echo -ne '\007'
+    SLEEPTIME=1
+    MESSAGE_COLOR=$red
+  elif [ $PRICE_DISCOUNT -ge 35 ]; then
+    SLEEPTIME=2
+    MESSAGE_COLOR=$red
+  elif [ $PRICE_DISCOUNT -ge 30 ]; then
+    SLEEPTIME=15
+    MESSAGE_COLOR=$yellow
+  elif [ $PRICE_DISCOUNT -ge 20 ]; then
+    SLEEPTIME=30
+    MESSAGE_COLOR=$green
+  elif [ $PRICE_DISCOUNT -le 10 ]; then
+    if [ $SLEEPTIME -lt 60 ]; then
+      SLEEPTIME=60
+      MESSAGE_COLOR=$reset
+      echo "\n**********************************************************************"
+    fi
+  fi
+  sleep $SLEEPTIME
+done
